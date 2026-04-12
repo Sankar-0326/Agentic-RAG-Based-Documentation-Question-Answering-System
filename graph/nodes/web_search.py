d0abd8d30c73601dbcecb19bdb6df71b3d0a5ca0
Sankar <veerd630478@gmail.com>
2026-04-12 13:57:55 +0530

    implemented web_search node

---
+++ graph/nodes/web_search.py
@@ -0,0 +1,31 @@
+from typing import Any, Dict
+
+from langchain_core.documents import Document
+from langchain_tavily import TavilySearch
+
+from graph.state import GraphState
+
+
+web_search_tool = TavilySearch(max_results= 3)
+
+
+def web_search(state: GraphState) -> Dict[str, Any] :
+    print("---WEB SEARCH---")
+    question = state["question"]
+    documents = state["documents"]
+
+    docs = web_search_tool.invoke({"query": question})
+    web_results = "\n".join([doc["content"] for doc in docs])
+    web_result = Document(page_content= web_results)
+
+    if documents is not None :
+        documents.append(web_result)
+    else :
+        documents = [web_result]
+
+    return {"documents": documents, "question": question}
+
+
+
+
+
