f76dcc5335da5bf7c0ff1a0d72f1acf5e92b007b
Sankar <veerd630478@gmail.com>
2026-04-12 17:20:06 +0530

    implement generate node

---
+++ graph/chains/generation.py
@@ -0,0 +1,11 @@
+from langsmith import Client
+from langchain_core.output_parsers import StrOutputParser
+from langchain_openai import ChatOpenAI
+
+llm = ChatOpenAI(temperature= 0)
+
+client = Client()
+prompt = client.pull_prompt("rlm/rag-prompt")
+
+generation_chain = prompt | llm
+
