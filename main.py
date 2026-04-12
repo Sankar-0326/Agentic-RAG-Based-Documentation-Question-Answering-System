477cdfa3b0838d8863db1426400322ae592178a5
Sankar <veerd630478@gmail.com>
2026-04-12 17:46:00 +0530

    Built LangGraph Stage-1

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+
+from graph.graph import app
+
 def main():
     print("Hello from agentic-rag!")
 
 
 if __name__ == "__main__":
     main()
+    print(app.invoke(input={"question": "what is agent memory?"}))
